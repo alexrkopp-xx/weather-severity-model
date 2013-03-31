3f2cfc4524b81a40b554b8aa43edead68a9045a5
Alex Kopp <alex.ryan.kopp@gmail.com>
2013-03-31 15:32:05 -0400

    Tweaked matching algorithm after last meeting. Saving feature matrix and feature names to files as well

--- match_alerts.py
+++ match_alerts.py
@@ -14,10 +14,22 @@ writer = csv.writer(w, delimiter=',')
 con = mdb.connect(mysql_host, mysql_user, mysql_pass, mysql_db)
 with con:
     cur = con.cursor()
-    cur.execute('SELECT * FROM `weather-severity`.cap c JOIN `weather-severity`.cap_fips capfips ON capfips.cap = c.id \
-                 JOIN `weather-severity`.storm_events se ON se.fips = capfips.fips \
-                 WHERE ABS(TIMESTAMPDIFF(HOUR,c.begin_time,se.begin_time)) <= 3 \
-                 AND ABS(TIMESTAMPDIFF(HOUR,c.expires,se.end_time)) <= 3')
+    #cur.execute('SELECT * FROM `weather-severity`.cap c JOIN `weather-severity`.cap_fips capfips ON capfips.cap = c.id \
+    #             JOIN `weather-severity`.storm_events se ON se.fips = capfips.fips \
+    #             WHERE (c.begin_time >= se.begin_time AND c.expires <= se.end_time) \
+    #             OR ( ABS(TIMESTAMPDIFF(MINUTE,c.begin_time,se.begin_time)) <= 0.25 * TIMESTAMPDIFF(MINUTE,se.end_time,se.begin_time) \
+    #             AND ABS(TIMESTAMPDIFF(MINUTE,c.expires,se.end_time)) <= 0.25 * TIMESTAMPDIFF(MINUTE,se.end_time,se.begin_time))')
+    cur.execute('SELECT * FROM `weather-severity`.cap c JOIN `weather-severity`.cap_fips capfips ON capfips.cap = c.id '
+                 'JOIN `weather-severity`.storm_events se ON se.fips = capfips.fips '
+                 'WHERE (c.begin_time >= se.begin_time AND c.expires <= se.end_time) '
+                 'OR ( ABS(TIMESTAMPDIFF(MINUTE,c.begin_time,se.begin_time)) <= 0.25 * TIMESTAMPDIFF(MINUTE,se.end_time,se.begin_time) '
+                 'AND ABS(TIMESTAMPDIFF(MINUTE,c.expires,se.end_time)) <= 0.25 * TIMESTAMPDIFF(MINUTE,se.end_time,se.begin_time)) '
+                 'OR (se.begin_time >= c.begin_time AND se.end_time <= c.expires) '
+                 'OR ( ABS(TIMESTAMPDIFF(MINUTE,c.begin_time,se.begin_time)) <= 0.25 * TIMESTAMPDIFF(MINUTE,c.expires,c.begin_time) '
+                 'AND ABS(TIMESTAMPDIFF(MINUTE,c.expires,se.end_time)) <= 0.25 * TIMESTAMPDIFF(MINUTE,c.expires,c.begin_time)) '
+    )
+    # CAP Fully Contained in Storm Event OR CAP start date is within a margin of 20% of the duration of the storm event
+
     for row in cur:
         writer.writerow(row)
 
--- test/feature_extraction.py
+++ test/feature_extraction.py
@@ -6,11 +6,44 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import DictVectorizer
 import nltk
 import re
+import cPickle as pickle
+import numpy as np
 
 stemmer = nltk.stem.PorterStemmer()
 def stemming_tokenizer(text):
     return [stemmer.stem(w).replace(".", "") for w in nltk.word_tokenize(text.lower())
             if re.match(r"\w", w)]
+
+# Modified from default english list in scikit-learn
+stopword_list = frozenset(
+    ['all', 'less', 'being', 'indeed', 'over', 'anyway', 'not', 'own', 'through', 'yourselves', 'fify', 'where', 'mill',
+     'only', 'find', 'before', 'whose', 'system', 'how', 'somewhere', 'with', 'show', 'had', 'enough', 'should', 'to',
+     'whom', 'seeming', 'ours', 'has', 'might', 'thereafter',
+     'latterly', 'do', 'them', 'his', 'around', 'than', 'get', 'very', 'de', 'none', 'cannot', 'every', 'whether',
+     'they', 'front', 'during', 'thus', 'now', 'him', 'nor', 'name', 'several', 'hereafter', 'always', 'who', 'cry',
+     'whither', 'this', 'someone', 'either', 'each', 'become', 'thereupon', 'sometime', 'side', 'therein',
+     'because', 'often', 'our', 'eg', 'some', 'back', 'up', 'go', 'namely', 'towards', 'are',
+     'further', 'beyond', 'ourselves', 'yet', 'out', 'even', 'will', 'what', 'still', 'for', 'bottom', 'mine', 'since',
+     'please', 'per', 'its', 'everything', 'behind', 'un', 'above', 'between', 'it', 'neither', 'seemed',
+     'ever', 'across', 'she', 'somehow', 'be', 'we', 'full', 'never', 'however', 'here', 'otherwise', 'were',
+     'whereupon', 'nowhere', 'although', 'found', 'alone', 're', 'along', 'by', 'both', 'about', 'last',
+     'would', 'anything', 'via', 'many', 'could', 'thence', 'put', 'against', 'keep', 'etc', 'amount', 'became', 'ltd',
+     'hence', 'onto', 'or', 'con', 'among', 'already', 'co', 'afterwards', 'formerly', 'within', 'seems', 'into',
+     'others', 'while', 'whatever', 'except', 'down', 'hers', 'everyone', 'done', 'least', 'another', 'whoever',
+     'moreover', 'couldnt', 'throughout', 'anyhow', 'yourself', 'from', 'her', 'few', 'together', 'top',
+     'there', 'due', 'been', 'next', 'anyone', 'much', 'call', 'therefore', 'interest', 'then', 'thru',
+     'themselves', 'was', 'sincere', 'empty', 'more', 'himself', 'elsewhere', 'mostly', 'on', 'am',
+     'becoming', 'hereby', 'amongst', 'else', 'part', 'everywhere', 'too', 'herself', 'former', 'those', 'he', 'me',
+     'myself', 'made', 'these', 'bill', 'cant', 'us', 'until', 'besides', 'nevertheless', 'below', 'anywhere',
+     'can', 'of', 'your', 'toward', 'my', 'something', 'and', 'whereafter', 'whenever', 'give', 'almost',
+     'wherever', 'is', 'describe', 'beforehand', 'herein', 'an', 'as', 'itself', 'at', 'have', 'in', 'seem', 'whence',
+     'ie', 'any', 'fill', 'again', 'hasnt', 'inc', 'thereby', 'thin', 'no', 'perhaps', 'latter', 'meanwhile', 'when',
+     'detail', 'same', 'wherein', 'beside', 'also', 'that', 'other', 'take', 'which', 'becomes', 'you', 'if', 'nobody',
+     'see', 'though', 'may', 'after', 'upon', 'most', 'hereupon', 'but', 'nothing', 'such', 'why',
+     'a', 'off', 'whereby', 'i', 'whole', 'noone', 'sometimes', 'well', 'amoungst', 'yours', 'their', 'rather',
+     'without', 'so', 'the', 'whereas', 'once'])
+
+
 
 logging.basicConfig(format='%(asctime)s;%(levelname)s:%(message)s', level=logging.DEBUG)
 mysql_host = 'localhost'
@@ -68,36 +101,41 @@ with con:
 
         row['fips'] = str(row['fips']) # Convert FIPS to string so we get dummy variables
         lstFeatures.append(row)
-        # Targets
-        #target_damage = property_damage + crop_damage
-        #target_injuries = injuries_direct + injuries_indirect
-        #target_deaths = deaths_direct + deaths_indirect
-
-        # Extract Features
-        # Categ
 
     # Part 1
     fvec = DictVectorizer()
     arrFeatures = fvec.fit_transform(lstFeatures) # Important to keep in sparse matrix
-    print fvec.get_feature_names()
+    print "(Part 1) Writing Feature Names to feature_names_1.dat"
+
+    with open('features_names_1.dat', 'wb') as outfile:
+        pickle.dump(fvec.get_feature_names(), outfile, pickle.HIGHEST_PROTOCOL)
 
     # Going to be used for the next few parts
-    freeTextVectorizer = TfidfVectorizer(charset_error='replace', tokenizer=stemming_tokenizer, analyzer='word', ngram_range=(2,2), stop_words='english', lowercase=True, max_features=None, norm='l2', use_idf=True, smooth_idf=True)
+    freeTextVectorizer = TfidfVectorizer(charset_error='replace', tokenizer=stemming_tokenizer, analyzer='word', ngram_range=(1,2), stop_words=stopword_list, lowercase=True, max_features=None, norm='l2', use_idf=True, smooth_idf=True)
 
     # Part 2 (Headlines)
     headlineFeatures = freeTextVectorizer.fit_transform(lstHeadlines)
     print headlineFeatures.shape
-    #print freeTextVectorizer.get_feature_names()
+    print "(Part 2) Writing Feature Names to feature_names_2.dat"
+
+    with open('feature_names_2.dat', 'wb') as outfile:
+        pickle.dump(freeTextVectorizer.get_feature_names(), outfile, pickle.HIGHEST_PROTOCOL)
 
     # Part 3 (Descriptions)
     descriptionFeatures = freeTextVectorizer.fit_transform(lstDescriptions)
     print descriptionFeatures.shape
-    #print freeTextVectorizer.get_feature_names()
+    print "(Part 3) Writing Feature Names to feature_names_3.dat"
+
+    with open('feature_names_3.dat', 'wb') as outfile:
+        pickle.dump(freeTextVectorizer.get_feature_names(), outfile, pickle.HIGHEST_PROTOCOL)
 
     # Part 4 (Instructions)
     instructionFeatures = freeTextVectorizer.fit_transform(lstInstructions)
     print instructionFeatures.shape
-    #print freeTextVectorizer.get_feature_names()
+    print "(Part 4) Writing Feature Names to feature_names_4.dat"
+
+    with open('feature_names_4.dat', 'wb') as outfile:
+        pickle.dump(freeTextVectorizer.get_feature_names(), outfile, pickle.HIGHEST_PROTOCOL)
 
     # Combine the parts
     features = hstack([arrFeatures,headlineFeatures])
@@ -105,4 +143,11 @@ with con:
     features = hstack([features,instructionFeatures])
 
     print features.shape
+
+    print "Writing Feature Matrix to features.dat"
+    with open('features.dat', 'wb') as outfile:
+        pickle.dump(features, outfile, pickle.HIGHEST_PROTOCOL)
+
+
+
     #print features.todense()
