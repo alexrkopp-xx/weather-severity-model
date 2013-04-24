657dea18fcfd3d414248dee3cbf542fb540a8c71
Alex Kopp <alex.ryan.kopp@gmail.com>
2013-04-24 09:28:26 -0400

    SGD Grid Search tweaking parameters

--- utils/train_sgdr_grid.py
+++ utils/train_sgdr_grid.py
@@ -22,8 +22,8 @@ feature_matrix = feature_matrix.tocsr()
 print feature_matrix.shape
 
 parameters = {'loss': ('squared_loss', 'huber'), 'penalty': ('l2', 'l1', 'elasticnet'),
-              'alpha': [1.00000000e+01, 1.00000000e+02, 1.00000000e+03, 1.00000000e+04, 1.00000000e+05, 1.00000000e+06],
-              'n_iter': [5, 10]}
+              'eta0': [0.1, 0.01, 0.001, 0.0001],
+              'n_iter': [300]}
 
 for target in ['property_damages', 'direct_deaths', 'indirect_deaths', 'direct_injuries',
                'indirect_injuries', 'crop_damages']:
@@ -36,8 +36,8 @@ for target in ['property_damages', 'direct_deaths', 'indirect_deaths', 'direct_i
     target_matrix = np.array(target_matrix)
 
     kf = KFold(len(target_matrix), n_folds=3, indices=True, shuffle=True)
-    sgdr = SGDRegressor(shuffle=True, verbose=1, epsilon=1.0)
-    gs = GridSearchCV(sgdr, parameters, n_jobs=-1, cv=kf, verbose=999, refit=False)
+    sgdr = SGDRegressor(shuffle=True, verbose=3)
+    gs = GridSearchCV(sgdr, parameters, n_jobs=-1, cv=kf, verbose=1, refit=False)
     gs.fit(feature_matrix, target_matrix)
 
     logger.info(gs.grid_scores_)
